d8235472a163fb73bbdb957a507cc4bed071b079
Steve Faulkner <faulkner.steve@gmail.com>
2023-08-28 21:59:43 +0100

    Update code.js

--- code.js
+++ code.js
@@ -1,99 +1,84 @@
 // Self-invoking function to prevent polluting the global namespace
-(function() {
+(function () {
+  // Function to get the center of an element
+  function getCenter(el) {
+    const rect = el.getBoundingClientRect();
+    return {
+      top: rect.top + window.scrollY + rect.height / 2,
+      left: rect.left + window.scrollX + rect.width / 2
+    };
+  }
 
-    // Function to compute the center of an element, taking into account the current scroll position.
-    function getCenter(el) {
-        const rect = el.getBoundingClientRect();
-        return {
-            top: rect.top + window.scrollY + rect.height / 2,
-            left: rect.left + window.scrollX + rect.width / 2
-        };
+  // Function to check if an element or its ancestors are hidden
+  function isVisible(el) {
+    let current = el;
+    while (current) {
+      const style = getComputedStyle(current);
+      if (style.display === 'none' || style.visibility === 'hidden') {
+        return false;
+      }
+      current = current.parentElement;
     }
+    return true;
+  }
 
-    // Function to check if an element, or any of its parents, have `display: none` or `visibility: hidden`
-    function isVisible(el) {
-        let current = el;
-        while (current) {
-            const style = getComputedStyle(current);
-            if (style.display === 'none' || style.visibility === 'hidden') {
-                return false;
-            }
-            current = current.parentElement;
-        }
-        return true;
-    }
+  const SVG_NS = 'http://www.w3.org/2000/svg';
+  
+  // Get all interactive elements that are visible
+  const elements = [...document.querySelectorAll('a, button, input:not([type=hidden]), select, textarea, [tabindex], [role=button], [role=checkbox], [role=link], [role=menuitem], [role=option], [role=radio], [role=switch], [role=tab]')].filter(isVisible);
+  
+  const centers = [];
 
-    // Define SVG namespace
-    const SVG_NS = "http://www.w3.org/2000/svg";
+  // Iterate through all interactive elements to find their centers and create SVG circles around them
+  elements.forEach(el => {
+    const center = getCenter(el);
+    centers.push({ element: el, center: center });
 
-    // Get all interactive elements (like buttons, links, etc.) on the page and filter out the ones that are not visible.
-    const elements = [...document.querySelectorAll("a, button, input:not([type=hidden]), select, textarea, [tabindex], [role=button], [role=checkbox], [role=link], [role=menuitem], [role=option], [role=radio], [role=switch], [role=tab]")].filter(isVisible);
+    const svg = document.createElementNS(SVG_NS, 'svg');
+    svg.style.position = 'absolute';
+    svg.style.top = `${center.top - 12}px`;
+    svg.style.left = `${center.left - 12}px`;
+    svg.style.width = '24px';
+    svg.style.height = '24px';
+    svg.style.zIndex = '9999';
+    svg.style.margin = '0';
+    svg.style.pointerEvents = 'none';
+    svg.setAttribute('aria-hidden', 'true');
+
+    const circle = document.createElementNS(SVG_NS, 'circle');
+    circle.setAttribute('cx', '12');
+    circle.setAttribute('cy', '12');
+    circle.setAttribute('r', '12');
     
-    // Array to store center points of all elements
-    const centers = [];
+    if (el.getBoundingClientRect().width < 24 || el.getBoundingClientRect().height < 24) {
+      circle.setAttribute('fill', 'rgba(255, 0, 0, 0.3)');
+      circle.setAttribute('stroke', 'rgba(255, 0, 0, 0.8)');
+      circle.setAttribute('stroke-width', '1');
+      circle.setAttribute('stroke-dasharray', '2,2');
+    } else {
+      circle.setAttribute('fill', 'rgba(0, 0, 255, 0.3)');
+    }
 
-    // Iterate over all elements, compute their centers, and draw SVG circles around them
-    elements.forEach(element => {
-        const center = getCenter(element);
-        centers.push({
-            element: element,
-            center: center
-        });
+    svg.appendChild(circle);
+    document.body.appendChild(svg);
+  });
 
-        // Create an SVG element for the circle
-        const svgElem = document.createElementNS(SVG_NS, "svg");
-        svgElem.style.position = "absolute";
-        svgElem.style.top = `${center.top - 12}px`;
-        svgElem.style.left = `${center.left - 12}px`;
-        svgElem.style.width = "24px";
-        svgElem.style.height = "24px";
-        svgElem.style.zIndex = "9999";
-        svgElem.style.padding = "0";
-        svgElem.style.margin = "0"; // found lobotomized owl CSS *-* messed with positioning, this resolves. Damn you Pickering!
-        svgElem.style.pointerEvents = "none";  // This ensures that the SVG won't interfere with user interactions
-        svgElem.setAttribute("aria-hidden", "true");  // Hide SVG from screen readers
+  const overlaps = [];
 
-        // Create a circle within the SVG
-        const circleElem = document.createElementNS(SVG_NS, "circle");
-        circleElem.setAttribute("cx", "12");
-        circleElem.setAttribute("cy", "12");
-        circleElem.setAttribute("r", "12");
-
-        // If the element is smaller than 24x24 pixels, give the circle a red fill and a dashed border. Otherwise, fill it blue.
-        if(element.getBoundingClientRect().width < 24 || element.getBoundingClientRect().height < 24) {
-            circleElem.setAttribute("fill", "rgba(255, 0, 0, 0.3)");
-            circleElem.setAttribute("stroke", "rgba(255, 0, 0, 0.8)");
-            circleElem.setAttribute("stroke-width", "1");
-            circleElem.setAttribute("stroke-dasharray", "2,2");
-        } else {
-            circleElem.setAttribute("fill", "rgba(0, 0, 255, 0.3)");
-        }
-
-        // Append the circle to the SVG, and the SVG to the document
-        svgElem.appendChild(circleElem);
-        document.body.appendChild(svgElem);
+  // Check for overlapping elements
+  centers.forEach((item1, index) => {
+    centers.slice(index + 1).forEach(item2 => {
+      if (Math.sqrt(Math.pow(item2.center.left - item1.center.left, 2) + Math.pow(item2.center.top - item1.center.top, 2)) < 24) {
+        overlaps.push(item1.element);
+        overlaps.push(item2.element);
+      }
     });
+  });
 
-    // List to store overlapping elements
-    const overlaps = [];
+  // Remove duplicates and set aria-description to 'overlap'
+  const uniqueOverlaps = [...new Set(overlaps)];
+  uniqueOverlaps.forEach(el => el.setAttribute('aria-description', 'overlap'));
 
-    // Check for overlaps between all pairs of elements
-    centers.forEach((a, i) => {
-        centers.slice(i+1).forEach(b => {
-            // If the distance between the centers of two elements is less than 24 pixels, they overlap.
-            if(Math.sqrt(Math.pow(b.center.left - a.center.left, 2) + Math.pow(b.center.top - a.center.top, 2)) < 24) {
-                overlaps.push(a.element);
-                overlaps.push(b.element);
-            }
-        });
-    });
-
-    // Filter out duplicate overlapping elements
-    const uniqueOverlaps = [...new Set(overlaps)];
-
-    // Add the 'aria-description' attribute to all overlapping elements
-    uniqueOverlaps.forEach(e => e.setAttribute("aria-description", "overlap"));
-
-    // Alert the number of overlapping controls
-    alert(`There are ${uniqueOverlaps.length} overlapping controls.`);
+  // Alert the user about the number of overlapping elements
+  alert(`There are ${uniqueOverlaps.length} overlapping controls.`);
 })();
